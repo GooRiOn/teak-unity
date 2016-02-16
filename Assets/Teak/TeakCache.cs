2e50159638eb086311846c4913e253e4e5e0e100
Pat Wilson <zerostride@gmail.com>
2016-02-16 14:32:14 -0800

    Update copyright.

--- Assets/Teak/TeakCache.cs
+++ Assets/Teak/TeakCache.cs
@@ -1,5 +1,5 @@
 #region License
-/* Teak -- Copyright (C) 2014 GoCarrot Inc.
+/* Teak -- Copyright (C) 2016 GoCarrot Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
